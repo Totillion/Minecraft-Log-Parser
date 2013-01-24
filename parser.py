79001a679a19a01149e14b28a17eb3f0844a9ca7
Isaac Schwarz <isaac@fogcreek.com>
2013-01-24 16:55:16 -0500

    Classify each users's time data, added in a last_seen time in output, handle the file open operation better as well as handle the case of no argument provided better

--- parser.py
+++ parser.py
@@ -1,50 +1,19 @@
 #!/usr/bin/env python
-import re, datetime, operator, sys
+import re
+import datetime
+import operator
+import sys
+import os
 
-actions = {
-    "login": re.compile("([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-2][0-9])\:([0-9]{2})\:([0-9]{2}) \[INFO\] ([A-z0-9]*) ?\[\/[0-9.]{4,15}\:[0-9]*\]"),
-    "logout": re.compile("([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-2][0-9])\:([0-9]{2})\:([0-9]{2}) \[INFO\] ([A-z0-9]*) lost connection")
-}
 
-path = "server.log"
-if sys.argv[1]:
-    path = sys.argv[1]
+class Player():
+    name = None
+    play_time = 0
+    last_seen = None
 
-f = open(path)
 
-online = {}
-totals = {}
-for line in f.readlines():
-    regex = None
-    action = None
-    player = None
-    time = None
-    for action in actions:
-        if actions[action].match(line):
-            regex = actions[action]
-            break
-
-    if regex is not None:
-        # Get the user's name and parse the datetime
-        data = regex.split(line)
-        player = data[7]
-        time = datetime.datetime(int(data[1]), int(data[2]), int(data[3]), int(data[4]), int(data[5]), int(data[6]))
-
-        # Now do things!
-        if action is "login":
-            online[player] = time
-        elif action is "logout":
-            if player not in totals:
-                totals[player] = 0
-            delta = time - online[player]
-            totals[player] += delta.seconds
-
-sort = sorted(totals.iteritems(), key=operator.itemgetter(1))
-times = []
-for player in sort:
-    # Convert to hours/minutes/seconds
-    time = "%20s: " % player[0]
-    total = player[1]
+def build_pretty_time_string(total):
+    time = "%20s | " % player
     days = total / 86400
     if days > 0:
         time += "%2s days" % int(days)
@@ -65,13 +34,74 @@ for player in sort:
         time += "           "
     if total > 0:
         time += " %2s seconds" % total
+    return time
 
-    times.append(time)
+actions = {
+    "login": re.compile("([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-2][0-9])\:([0-9]{2})\:([0-9]{2}) \[INFO\] ([A-z0-9]*) ?\[\/[0-9.]{4,15}\:[0-9]*\]"),
+    "logout": re.compile("([0-9]{4})\-([0-9]{2})\-([0-9]{2}) ([0-2][0-9])\:([0-9]{2})\:([0-9]{2}) \[INFO\] ([A-z0-9]*) lost connection")
+}
 
-times.reverse()
+server_log = 'server.log'
 
-counter = 0
-for time in times:
-    counter =+ 1
-    print "%2d) %s" % (counter, time)
+try:
+    server_log = sys.argv[1]
+except IndexError: # no argument supplied in the command prompt, then see if we find server.log in the current dir
+    if os.path.exists(os.path.join(os.getcwd(), 'server.log')):
+        server_log = os.path.join(os.getcwd(), 'server.log')
+
+if os.path.exists(server_log):
+    with open(server_log, 'r') as f: #open file then close it
+        file_contents = f.readlines()
+else:
+    print 'Could not locate "server.log" - try adding the full path to the log file as an argument, or simply running ' \
+          'this script in the same directory as the server.log file'
+
+online = {}
+totals = {}
+
+for line in file_contents:
+    regex = None
+    action = None
+    player = None
+    time = None
+    for action in actions:
+        if actions[action].match(line):
+            regex = actions[action]
+            break
+
+    if regex:
+        # Get the user's name and parse the datetime
+        data = regex.split(line)
+        player = data[7]
+        time = datetime.datetime(int(data[1]), int(data[2]), int(data[3]), int(data[4]), int(data[5]), int(data[6]))
+
+        # Now do things!
+        if action is "login":
+            online[player] = time
+
+            if player not in totals:
+                totals[player] = Player()
+
+        elif action is "logout":
+            delta = time - online[player]
+            totals[player].play_time += delta.seconds
+
+            # Get the last logged in date/time
+            if totals[player].last_seen:
+                if time > totals[player].last_seen:
+                    totals[player].last_seen = time
+            else:
+                totals[player].last_seen = time
+
+results = []
+
+for player in totals:
+    pretty_time_string = build_pretty_time_string(totals[player].play_time)
+    results.append([pretty_time_string, totals[player].last_seen, totals[player].name, totals[player].play_time])
+
+s_results = sorted(results, key=operator.itemgetter(3))
+s_results.reverse()
+
+for res in s_results:
+    print "{0}) {1} | Last Seen On: {2}".format(s_results.index(res) + 1, res[0], res[1])
 f.close()
